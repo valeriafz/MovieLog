bcb74b18ceed425e477f9c5421408a2768bf70ef
valeriafz <valeria.frunza33@gmail.com>
2025-04-24 15:12:49 +0300

    add filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
 import Layout from '@/components/Layout';
 import MovieCard from '@/components/MovieCard';
+import { useState } from 'react';
 
 export default function Home() {
+  const [showFilters, setShowFilters] = useState(false);
+  const [activeFilter, setActiveFilter] = useState<string | null>(null); // Update type here
+
+  const filters = [
+    { label: 'Watch later', value: 'watch-later' },
+    { label: 'Completed', value: 'completed' },
+    { label: 'Higher reviewed', value: 'higher-reviewed' },
+    { label: 'Lower reviewed', value: 'lower-reviewed' }
+  ];
+
   return (
     <Layout>
       <div className="relative mx-auto w-1/2 -mb-4 z-20"> 
@@ -16,11 +29,37 @@ export default function Home() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </div>
-          <div className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600 cursor-pointer">
+          <div 
+            className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-700 cursor-pointer"
+            onClick={() => setShowFilters(!showFilters)}
+          >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
             </svg>
           </div>
+          
+          {showFilters && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
+              <div className="py-1">
+                {filters.map((filter) => (
+                  <div
+                    key={filter.value}
+                    className={`block px-4 py-2 text-sm cursor-pointer ${
+                      activeFilter === filter.value 
+                        ? 'bg-white text-gray-900' 
+                        : 'hover:font-semibold hover:bg-gray-100'
+                    }`}
+                    onClick={() => {
+                      setActiveFilter(filter.value);
+                      setShowFilters(false);
+                    }}
+                  >
+                    {filter.label}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
       
