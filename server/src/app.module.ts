e4999b11c595eb8a0260e5610576ff28fd9bd4f2
valeriafz <valeria.frunza33@gmail.com>
2025-05-07 20:52:14 +0300

    add logs for succesfull connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppService } from './app.service';
 import databaseConfig from './config/database.config';
 
 @Module({
@@ -13,14 +14,15 @@ import databaseConfig from './config/database.config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('database.uri'),
-        dbName: configService.get<string>('database.database'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('database.uri');
+        console.log(`Attempting to connect to MongoDB at ${uri}`);
+        return {
+          uri,
+        };
+      },
     }),
   ],
-  
-  
-
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ server/src/app.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class AppService implements OnModuleInit {
+  private dbConnectionStatus = false;
+
+  constructor(
+    @InjectConnection() private readonly connection: Connection,
+    private configService: ConfigService,
+  ) {
+    
+  }
+
+  async onModuleInit() {
+    try {
+      this.dbConnectionStatus = this.connection.readyState === 1;
+      if (this.dbConnectionStatus) {
+        console.log('Successfully connected to MongoDB database');
+      } else {
+        console.error('Failed to connect to MongoDB database');
+      }
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      this.dbConnectionStatus = false;
+    }
+  }
+
+  getHello(): string {
+    return 'Welcome to MovieLog API!';
+  }
+
+  getDatabaseStatus(): { status: string; } {
+    return {
+      status: this.dbConnectionStatus ? 'Connected' : 'Disconnected',
+    };
+  }
+}
